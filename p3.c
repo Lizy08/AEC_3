b2e71c67888cc3f950de3fc7c0fb99c9adf77c48
lizy <lizybarboza31@gmail.com>
2024-07-29 09:39:05 +0400

    make changes to all files

---
+++ p3.c
@@ -0,0 +1,14 @@
+// Simple C program to display "Hello World"
+ 
+// Header file for input output functions
+#include <stdio.h>
+ 
+// main function -
+// where the execution of program begins
+int main()
+{
+    x=2
+    printf(x);
+ 
+    return 0;
+}
